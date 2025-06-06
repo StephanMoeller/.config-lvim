5692e357b282121121e451fabd8bb9ec30ef5f8f
Stephan Moeller <stephanryer@hotmail.com>
2025-06-06 22:35:47 +0200

    added files

---
+++ config.lua
@@ -0,0 +1,80 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Example configs: https://github.com/LunarVim/starter.lvim
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+lvim.plugins = {
+  {
+    "lunarvim/colorschemes",
+    config = function()
+      lvim.colorscheme = "catppuccin-frappe"
+    end
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme "catppuccin"
+    end
+  },
+  {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+      "TmuxNavigatorProcessList",
+    },
+    config = function()
+
+    end
+  }
+}
+
+vim.wo.relativenumber = true
+vim.wo.number = true
+
+-- PROGRAMMING: Add comma and semicolon to the end of the current line
+vim.keymap.set('n', 'K', 'mzJ`z', { desc = 'Join lines' })
+vim.keymap.set('n', 'k', 'f,wi<enter><esc>', { desc = 'Split lines' })
+vim.keymap.set('n', 'U', '<c-r>', { desc = 'Redo' })
+vim.keymap.set({ 'n', 'v' }, '<leader>y', '"+y', { desc = 'Copy to clipboard' })
+vim.keymap.set({ 'n', 'v' }, '<leader>p', '"+p', { desc = 'Paste from clipboard' })
+vim.keymap.set({ 'n', 'v' }, '<c-p>', '"0p', { desc = 'Paste last yanked' })
+vim.keymap.set('n', '*', '*zz', { desc = 'Center when *' })
+vim.keymap.set('n', 'n', 'nzz', { desc = 'Center when n' })
+
+-- Home and end go to first and last non space character
+vim.keymap.set({ 'n', 'v' }, '<Home>', '<Home>_', { desc = 'Home => first non space' })
+vim.keymap.set('i', '<Home>', '<esc><home>_i', { desc = 'Home => first non space' })
+vim.keymap.set('n', '<End>', 'g_', { desc = 'End => last non space' })
+vim.keymap.set('i', '<End>', '<esc>g_a', { desc = 'End => last non space' })
+vim.keymap.set('n', '<c-a>', 'ggvG', { desc = 'Select all' })
+vim.keymap.set('v', '<End>', 'g_', { desc = 'End => last non space' })
+vim.keymap.set('n', '<c-z>', 'zz', { desc = 'Select all' })
+vim.keymap.set({ 'n', 'v' }, '<c-Up>', '10<Up>zz', { desc = 'Navigate with crlt arrows instead of c-d and c-u' })
+vim.keymap.set({ 'n', 'v' }, '<c-Down>', '10<Down>', { desc = 'Navigate with crlt arrows instead of c-d and c-u' })
+
+vim.keymap.set('n', 'h', 'v', { desc = 'Use h instead of v for ergo reasons' })
+vim.keymap.set('n', 'H', 'V', { desc = 'Use h instead of v for ergo reasons' })
+
+vim.keymap.set({ 'n', 'v' }, '<C-S>', ':wa<Enter>', { desc = 'save all files' })
+vim.keymap.set('i', '<C-S>', '<Esc>:wa<Enter>', { desc = 'save all files' })
+
+vim.keymap.set('n', '<C-Q>', ':q!<Enter>', { desc = 'save all files' })
+
+
+vim.keymap.set('n', '<c-i>', '<c-o>', { desc = 'swap c-i and c-o' })
+vim.keymap.set('n', '<c-o>', '<c-i>', { desc = 'swap c-i and c-o' })
+
+
+vim.keymap.set({ 'n', 'v', 'i' }, '<m-left>', '<Esc>:TmuxNavigateLeft<CR>')
+vim.keymap.set({ 'n', 'v', 'i' }, '<m-right>', '<Esc>:TmuxNavigateRight<cr>')
+vim.keymap.set({ 'n', 'v', 'i' }, '<m-up>', '<Esc>:TmuxNavigateUp<cr>')
+vim.keymap.set({ 'n', 'v', 'i' }, '<m-down>', '<Esc>:TmuxNavigateDown<cr>')
+
+vim.keymap.set('n', ';', 'A;<ESC><HOME><DOWN>')
+vim.keymap.set('n', ',', 'A,<ESC><HOME><DOWN>')
